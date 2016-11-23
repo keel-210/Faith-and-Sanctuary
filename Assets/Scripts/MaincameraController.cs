072016ff451374e468f4eeb5377690009ccbdd22
keel-210 <crafters_touhou@yahoo.co.jp>
2016-11-24 00:54:41 +0900

    Ver0.0
    Making Tutorial
    Make Sanctuary

---
+++ Assets/Scripts/MaincameraController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MaincameraController : MonoBehaviour {
+    public Vector3 view;
+	void Update ()
+    {
+        transform.LookAt(view);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public char faith;
+    public int move = 1;
+
+    private GameObject sanc;
+    private int phase;
+	void Start () {
+        sanc = GameObject.Find("Sanctuaries");
+        phase = sanc.GetComponent<SanctuariesController>().phase;
+	}
+	
+	void Update () {
+        phase = sanc.GetComponent<SanctuariesController>().phase;
+        if (phase == 2)
+        {
+            Debug.Log("moved");
+            
+            if (move <= 3)
+            {
+                float movehor = Input.GetAxisRaw("Horizontal");
+                float movever = Input.GetAxisRaw("Vertical");
+                Vector3 movement = new Vector3(movehor, 0, movever);
+                move++;
+            }
+            else if(move > 3)
+            {
+                move = 0;
+                sanc.GetComponent<SanctuariesController>().phase += 1;
+            }
+            
+        }
+        
+	}
+}
