d7cc974b9eb80ed766bed6f54a16a3e43859ed5b
keel-210 <crafters_touhou@yahoo.co.jp>
2016-12-20 19:31:02 +0900

    F
    This reverts commit 79ad3765702ba85d3f3057d3c835c08af7adc18a.
    
    # Conflicts:
    #	.vs/Faith and Sanctuary/v14/.suo
    #	Library/ScriptAssemblies/Assembly-CSharp.dll
    #	Library/ScriptAssemblies/Assembly-CSharp.dll.mdb
    #	Library/assetDatabase3

--- Assets/Scripts/TutorialWaves.cs
+++ Assets/Scripts/TutorialWaves.cs
@@ -44,6 +44,7 @@ public class TutorialWaves : SanctuariesController {
                     {
                         HereIsSanctuary(1, 2, 'g');
                         HereIsSanctuary(1, 2, 'r');
+                        CrossIsSanctuary(0, 2, 'b');
                         SancHasDeployed = true;
                     }
                     Message();
@@ -100,7 +101,80 @@ public class TutorialWaves : SanctuariesController {
         }
         Instantiate(Resources.Load("Prefabs/HereIsSanc"), new Vector3(row * 2, 0.1f * (row + col) + 2.4f, col * 2), Quaternion.Euler(-90, -45, -90));
     }
-
+    void CrossIsSanctuary(int row, int col, char faith)
+    {
+        int Maxrow = transform.GetComponent<SizeOfSanc>().row;
+        int Maxcol = transform.GetComponent<SizeOfSanc>().column;
+        if (faith == 'r')
+        {
+            for(int i = 0; i < Maxrow; i++)
+            {
+                if(row - i >= 0)
+                {
+                    fields[row - i][col].red = true;
+                }
+                if (col - i >= 0)
+                {
+                    fields[row][col - i].red = true;
+                }
+                if(row + i < Maxrow)
+                {
+                    fields[row + i][col].red = true;
+                }
+                if(col + i < Maxcol)
+                {
+                    fields[row][col + i].red = true;
+                }
+            }
+            Instantiate(Resources.Load("Prefabs/CrossIsSanc"), new Vector3(row * 2, 0.1f * (row + col) + 2.4f, col * 2), Quaternion.Euler(-90, -45, -90));
+        }
+        if (faith == 'b')
+        {
+            for (int i = 0; i < Maxrow; i++)
+            {
+                if (row - i >= 0)
+                {
+                    fields[row - i][col].blue = true;
+                }
+                if (col - i >= 0)
+                {
+                    fields[row][col - i].blue = true;
+                }
+                if (row + i < Maxrow)
+                {
+                    fields[row + i][col].blue = true;
+                }
+                if (col + i < Maxcol)
+                {
+                    fields[row][col + i].blue = true;
+                }
+            }
+            Instantiate(Resources.Load("Prefabs/CrossIsSanc"), new Vector3(row * 2, 0.1f * (row + col) + 2.4f, col * 2), Quaternion.Euler(-90, -45, -90));
+        }
+        if (faith == 'g')
+        {
+            for (int i = 0; i < Maxrow; i++)
+            {
+                if (row - i >= 0)
+                {
+                    fields[row - i][col].green = true;
+                }
+                if (col - i >= 0)
+                {
+                    fields[row][col - i].green = true;
+                }
+                if (row + i < Maxrow)
+                {
+                    fields[row + i][col].green = true;
+                }
+                if (col + i < Maxcol)
+                {
+                    fields[row][col + i].green = true;
+                }
+            }
+            Instantiate(Resources.Load("Prefabs/CrossIsSanc"), new Vector3(row * 2, 0.1f * (row + col) + 2.4f, col * 2), Quaternion.Euler(-90, -45, -90));
+        }
+    }
     void Message()
     {
         message.Make(phase);
